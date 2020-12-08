0181be888f551e02eff12a4c16d55981090a6c1f
Josep <josep@localhost.localdomain>
2020-12-08 18:26:46 +0100

    primer commit

---
+++ scriptgroupby.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+dir=$1
+arrel=$(readlink -f $2)
+
+cd "$dir"
+
+for x in *; do
+    if [ -f "$x" ]; then
+        d=$(date -r "$x" +%Y/%m/%d)
+        mkdir -pv "$arrel/$d"
+        mv -v -- "$x" "$arrel/$d/"
+    fi
+done
+
+
+
+
